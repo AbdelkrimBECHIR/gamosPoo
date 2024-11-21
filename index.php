55e40849a16444d3cdac7163a95a9b8c523a4518
Abdelkrim BECHIR <abdel.bechir@yahoo.fr>
2024-11-21 12:08:52 +0100

    creation du routeur et init des controller et des action dans page index. php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require('routeurs.php');
+
+
+// on creer un routeur de class Routeur
+$router= new Router();
+// on instancie le tableau recuperer grace a get Controller en donnant en parametre l'uri
+$elements=$router->getController($_SERVER['REQUEST_URI']);
+
+$controller=$elements['controller'];
+
+//on initialise une instance pour connecter la bdd si besoin avec le nom du controller+'controller'
+$cont= new $Controller($dbh);
+
+// on appel l'action du controller en recuperant la valeur du tableau 
+$action=$elements['action'];
+//on utilise la methode du controller pour appliquer l'action
+$cont->$action();
+
+
+
+
